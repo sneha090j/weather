003c0f53a88c5c3bf797b3e26b33c623dd2b5f0e
sneha090j <sneha090j@gmail.com>
2024-07-19 22:19:06 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,41 +8,55 @@ const url = require("url");
 const homeFile = fs.readFileSync("home.html", "utf-8");
 
 const replaceVal = (tempVal, orgVal) => {
+    if (!orgVal.main || !orgVal.sys || !orgVal.weather) {
+        return tempVal.replace("{%tempval%}", "N/A")
+                      .replace("{%tempmin%}", "N/A")
+                      .replace("{%tempmax%}", "N/A")
+                      .replace("{%location%}", "Unknown")
+                      .replace("{%country%}", "Unknown")
+                      .replace("{%tempStatus%}", "N/A");
+    }
+
     let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
     temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
     temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
     temperature = temperature.replace("{%location%}", orgVal.name);
     temperature = temperature.replace("{%country%}", orgVal.sys.country);
     temperature = temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
+    console.log(`${ orgVal.weather[0].main}`);
     return temperature;
 };
 
 const server = http.createServer((req, res) => {
-    const reqUrl = url.parse(req.url).pathname;
+    const reqUrl = url.parse(req.url, true);
+    const pathname = reqUrl.pathname;
 
-    if (reqUrl === "/") {
-        requests(
-            "https://api.openweathermap.org/data/2.5/weather?q=pune&appid=a19cb112564b9350d7ca63f1b234cf37"
-        )
-        .on("data", function (chunk) {
-            const objdata = JSON.parse(chunk);
-            const arrdata = [objdata];
-            const realTimeData = arrdata
-                .map((val) => replaceVal(homeFile, val))
-                .join("");
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(realTimeData);
-        })
-        .on("end", function (err) {
-            if (err) {
-                console.log("Connection closed", err);
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end("Error fetching data");
-            } else {
-                res.end();
-            }
-        });
-    } else if (reqUrl === "/style.css") {
+    if (pathname === "/") {
+        const city = reqUrl.query.city ; // Default to 'pune' if no city is provided
+        console.log(`${city}`); // Debugging line
+        const apiKey = "a19cb112564b9350d7ca63f1b234cf37";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+        requests(apiUrl)
+            .on("data", function (chunk) {
+                const objdata = JSON.parse(chunk);
+                const arrdata = [objdata];
+                const realTimeData = arrdata
+                    .map((val) => replaceVal(homeFile, val))
+                    .join("");
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(realTimeData);
+            })
+            .on("end", function (err) {
+                if (err) {
+                    console.log("Connection closed", err);
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end("Error fetching data");
+                } else {
+                    res.end();
+                }
+            });
+    } else if (pathname === "/style.css") {
         // Serving the CSS file
         fs.readFile(path.join(__dirname, "style.css"), (err, data) => {
             if (err) {
